c3f576fd4188f462cd8b8a536daa061c822f76b0
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2023-05-22 10:36:30 +0300

    Try with different csp

--- client/add-csp-header.ts
+++ client/add-csp-header.ts
@@ -3,7 +3,7 @@ const fs = require("fs");
 const outputDir = "dist"; // Specify the output directory where the Next.js export is generated
 
 // CSP header configuration
-const cspHeader = "<meta http-equiv=\"Content-Security-Policy\" content=\"default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self';\">";
+const cspHeader = "<meta http-equiv=\"Content-Security-Policy\" content=\"default-src 'self'\">";
 
 // Function to add CSP header to HTML files
 const addCspHeader = (filePath: string) => {
